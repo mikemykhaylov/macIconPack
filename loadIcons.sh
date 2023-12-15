6b2168c1cf317374c58c8b7ab977e4307fb9a42b
Michael Mykhaylov <mikemikhailov2003@gmail.com>
2023-12-15 14:34:36 -0700

    Add fileicon installed check

--- loadIcons.sh
+++ loadIcons.sh
@@ -3,6 +3,14 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Script directory: $SCRIPT_DIR"
 
+# Check that fileicon is installed
+if ! command -v fileicon &> /dev/null
+then
+    echo "fileicon could not be found"
+    echo "Install with: brew install fileicon"
+    exit
+fi
+
 for file in "$SCRIPT_DIR"/icons/*.icns; do
     filename=$(basename "$file")
     filename="${filename%.*}"
